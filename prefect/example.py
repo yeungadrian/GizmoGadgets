6719f8ce817a4e8ff2a01afdd94e7a56c65b29c7
yeungadrian <yeungadrianjy@gmail.com>
2021-10-01 22:41:50 +0100

    Prefect secrets

--- prefect/example.py
+++ prefect/example.py
@@ -1,14 +1,21 @@
 from prefect import task, Flow
+from prefect.tasks.secrets import PrefectSecret
 from datetime import timedelta
 from prefect.schedules import IntervalSchedule
 
 @task
-def say_hello():
-    print("Hello, world!")
+def my_task(credentials):
+    """A task that requires credentials to access something. Passing the
+    credentials in as an argument allows you to change how/where the
+    credentials are loaded (though we recommend using `PrefectSecret` tasks to
+    load them."""
+    something = str(credentials)
+    print(something)
 
 schedule = IntervalSchedule(interval=timedelta(minutes=1))
 
 with Flow("Hello", schedule) as flow:
-    say_hello()
+    my_secret = PrefectSecret("USERNAME")
+    my_task(credentials=my_secret)
 
 flow.run()
