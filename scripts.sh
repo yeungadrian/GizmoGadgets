232fb4d1e5173a82b2872930b1ed2887d8afd640
yeungadrian <yeungadrianjy@gmail.com>
2021-10-02 18:59:58 +0100

    Prefect Task calling riot games api and saving results to postgres db

--- scripts.sh
+++ scripts.sh
@@ -2,4 +2,4 @@ prefect backend server
 prefect server create-tenant -n default
 prefect create project teamfighttactics
 prefect register -p prefect --project teamfighttactics
-prefect agent local starts
+prefect agent local start --env PREFECT__CONTEXT__SECRETS__APIKEY=
