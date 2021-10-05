f1164fccc98ae00a8c23b9b6b00533860fdbdf18
yeungadrian <yeungadrianjy@gmail.com>
2021-10-05 21:52:39 +0100

    Going through all endpoints in notebook

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -2,4 +2,4 @@ prefect backend server
 prefect server create-tenant -n default
 prefect create project teamfighttactics
 prefect register -p prefect --project teamfighttactics
-prefect agent local start --env PREFECT__CONTEXT__SECRETS__APIKEY=RGAPI-5c85443b-0bc0-4263-b969-286f84a34b05
+prefect agent local start --env PREFECT__CONTEXT__SECRETS__APIKEY=
