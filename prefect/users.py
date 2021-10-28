f37baa859f5b599f30f2d306edc92d40be26a739
yeungadrian <yeungadrianjy@gmail.com>
2021-10-28 23:01:52 +0100

    Refactoring of users, removing pandas

--- prefect/users.py
+++ prefect/users.py
@@ -1,41 +1,47 @@
 from prefect import task, Flow
 from prefect.tasks.secrets import PrefectSecret
 from prefect.tasks.postgres import PostgresExecute
-import psycopg2 as pg
 import requests
-import pandas as pd
 import time
 
+
 @task
-def update_challenger_league(username, password, db_name, api_key):
+def get_challenger_users(username, password, db_name, api_key):
     """A task that requires credentials to access something. Passing the
     credentials in as an argument allows you to change how/where the
     credentials are loaded (though we recommend using `PrefectSecret` tasks to
     load them."""
-    query_header = {
-            "X-Riot-Token": api_key
-    }
-    challenger_url = 'https://euw1.api.riotgames.com/tft/league/v1/challenger'
-    challenger_response = requests.get(url = challenger_url, headers=query_header).json()
-    challenger_response_df = pd.DataFrame(challenger_response['entries'])
 
-    for i in range(0, challenger_response_df.shape[0]):
-        summonerId = str(challenger_response_df["summonerId"][i])
-        summonerName = str(challenger_response_df["summonerName"][i])
-        leaguePoints = int(challenger_response_df["leaguePoints"][i])
+    query_header = {"X-Riot-Token": api_key}
 
-        puuid_url = f'https://euw1.api.riotgames.com/tft/summoner/v1/summoners/{summonerId}'
-        puuid_response = requests.get(url = puuid_url, headers=query_header).json()
+    challenger_url = "https://euw1.api.riotgames.com/tft/league/v1/challenger"
+    challenger_response = requests.get(url=challenger_url, headers=query_header).json()
+    time.sleep(2)
+
+    for i in challenger_response["entries"]:
+        summonerId = i["summonerId"]
+        summonerName = i["summonerName"]
+        leaguePoints = i["leaguePoints"]
+
+        puuid_url = (
+            f"https://euw1.api.riotgames.com/tft/summoner/v1/summoners/{summonerId}"
+        )
+        puuid_response = requests.get(url=puuid_url, headers=query_header).json()
+        time.sleep(2)
         puuid = str(puuid_response["puuid"])
 
-        time.sleep(1.6)
-
-        user_query = '''
+        user_query = """
             INSERT INTO public.users(
                 id, summonername, summonerid, rankedleague, puuid, leaguepoints, region)
                 VALUES (DEFAULT, %s, %s, 'CHALLENGER', %s, %s , 'EUW1')
-                '''
-        query_data = (summonerName, summonerId, puuid,leaguePoints,)
+                """
+
+        query_data = (
+            summonerName,
+            summonerId,
+            puuid,
+            leaguePoints,
+        )
 
         PostgresExecute(
             db_name=db_name,
@@ -47,12 +53,13 @@ def update_challenger_league(username, password, db_name, api_key):
             commit=True,
         ).run(password=password)
 
-
-with Flow("Update Challenger League") as flow:
+with Flow("Get Challenger Summoners") as flow:
     username = PrefectSecret("USERNAME")
     password = PrefectSecret("PASSWORD")
     api_key = PrefectSecret("APIKEY")
-    dbname= 'teamfighttactics' 
-    update_challenger_league(username = username, password = password,db_name = dbname, api_key = api_key)
+    dbname = "teamfighttactics"
+    get_challenger_users(
+        username=username, password=password, db_name=dbname, api_key=api_key
+    )
 
 flow.run()
